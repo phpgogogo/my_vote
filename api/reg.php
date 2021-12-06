a7a57e9c4a406a15f60a98b2052bad1b0b8bf5e2
Chang <cliff60309@gmail.com>
2021-12-06 08:40:55 +0800

    test

--- api/reg.php
+++ api/reg.php
@@ -1,14 +1,21 @@
 <?php
 include_once "db.php";
 
-echo "<pre>";
-print_r($_POST);
-echo "</pre>";
-echo $_POST["account"];
+// echo "<pre>";
+// print_r($_POST);
+// echo "</pre>";
+// echo $_POST["account"];
 // all("users",[""]);
 $sql="SELECT `account` FROM `users`";
 q($sql);
-
+// insert("users",$_POST);
+echo "<pre>";
+print_r(q($sql));
+echo "<pre>";
+// find("users",["id"=>"1"]);
+echo "<pre>";
+print_r(find("users",["id"=>"1"]));
+echo "<pre>";
 
 
 
