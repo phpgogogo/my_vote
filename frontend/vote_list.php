e682296fdf94d0f8f70cfe44223792f07a624b88
phpgogogo <cliff60309@gmail.com>
2021-12-13 01:23:44 +0800

    更新投票清單頁面(總票數未完成)

--- frontend/vote_list.php
+++ frontend/vote_list.php
@@ -14,9 +14,20 @@ if (isset($_SESSION["user"])) {
         echo $value["topic"];
         echo "</span>";
         
+        // 總投票數顯示
+        $count=q("SELECT sum(`count`) as '總計' FROM `options` WHERE `topic_id`='{$value['id']}'");
+        // dd($count);
+        echo "<span class='d-inline-block col-md-2 text-center'>";
+        echo $count[0]['總計'];
+        echo "</span>";
+        echo "<a href='?do=vote_result&id={$value['id']}' class='col-md-2 offset-md-4 text-center'>";
+        echo "<button class='btn btn-primary'>觀看結果</button>";
+        echo "</a>";
+        // 投票按鈕
         echo "<a href='../index.php?do=vote&id={$value['id']}' class='col-md-2 offset-md-2 text-center'>";
         echo "<button class='btn btn-success'>投票</button>";
         echo "</a>";
+        // 投票結果按鈕
         echo "<a href='?do=vote_result&id={$value['id']}' class='col-md-2 text-center'>";
         echo "<button class='btn btn-primary'>觀看結果</button>";
         echo "</a>";
