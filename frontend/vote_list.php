a21a04c1e0eadd2b6fdfa3b8659b3884c10992f0
phpgogogo <cliff60309@gmail.com>
2021-12-13 00:08:48 +0800

    更新投票清單頁面(總票數未完成)

--- frontend/vote_list.php
+++ frontend/vote_list.php
@@ -1,35 +1,52 @@
 <h1>投票清單</h1>
 
 <?php
-$subjects=all("topics");
-if(isset($_SESSION["user"])){
+$subjects = all("topics");
+if (isset($_SESSION["user"])) {
     echo "<ol class='list-group'>";
-foreach($subjects as $key=>$value){
-    // echo $value["topic"];
-    // echo "<br>";
-    // echo $value["id"];
-    // echo "<br>";
-    echo "<li>";
-    echo "<a href='../index.php?do=vote&id={$value['id']}'>";
-    echo $value["topic"];
-    echo "</a>";
-    echo "</li>";
-}
-echo "</ol>";
-}else{
+    foreach ($subjects as $key => $value) {
+        // echo $value["topic"];
+        // echo "<br>";
+        // echo $value["id"];
+        // echo "<br>";
+        echo "<li class='mt-2 row align-items-center'>";
+        echo "<span class='col-md-6 text-left'>";
+        echo $value["topic"];
+        echo "</span>";
+        
+        echo "<a href='../index.php?do=vote&id={$value['id']}' class='col-md-2 offset-md-2 text-center'>";
+        echo "<button class='btn btn-success'>投票</button>";
+        echo "</a>";
+        echo "<a href='?do=vote_result&id={$value['id']}' class='col-md-2 text-center'>";
+        echo "<button class='btn btn-primary'>觀看結果</button>";
+        echo "</a>";
+        echo "</li>";
+    }
+    echo "</ol>";
+} else {
     echo "<ol class='list-group'>";
-foreach($subjects as $key=>$value){
-    // echo $value["topic"];
-    // echo "<br>";
-    // echo $value["id"];
-    // echo "<br>";
-    echo "<li>";
-    echo "<span>";
-    echo $value["topic"];
-    echo "</span>";
-    echo "</li>";
-}
-echo "</ol>";
+    foreach ($subjects as $key => $value) {
+        // echo $value["topic"];
+        // echo "<br>";
+        // echo $value["id"];
+        // echo "<br>";
+        echo "<li class='mt-2 row align-items-center'>";
+        echo "<span class='col-md-6 text-left'>";
+        echo $value["topic"];
+        echo "</span>";
+        
+        // 總投票數顯示
+        $count=q("SELECT sum(`count`) as '總計' FROM `options` WHERE `topic_id`='{$value['id']}'");
+        // dd($count);
+        echo "<span class='d-inline-block col-md-2 text-center'>";
+        echo $count[0]['總計'];
+        echo "</span>";
+        echo "<a href='?do=vote_result&id={$value['id']}' class='col-md-2 offset-md-4 text-center'>";
+        echo "<button class='btn btn-primary'>觀看結果</button>";
+        echo "</a>";
+        echo "</li>";
+    }
+    echo "</ol>";
 }
 
 
@@ -37,9 +54,3 @@ echo "</ol>";
 
 
 ?>
-
-
-
-
-
-
