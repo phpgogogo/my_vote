39c3fcd61921cf40b38cc3d3306dbc793e5b13e8
phpgogogo <cliff60309@gmail.com>
2021-12-17 22:43:50 +0800

    登入和註冊頁面優化

--- frontend/login.php
+++ frontend/login.php
@@ -1,5 +1,5 @@
-<h1>登入頁面</h1>
-<h2 class="text-center">會員登入</h2>
+<!-- <h1>登入頁面</h1> -->
+<h2 class="text-center my-3 font-weight-bold">會員登入</h2>
 
 <form action="./api/check_login.php" method="post">
             <table id="loginForm" class="table w-auto m-auto">
@@ -12,7 +12,7 @@
                     <td><input type="password" name="password" id=""></td>
                 </tr>
                 <tr>
-                    <td colspan="2">
+                    <td colspan="2" class="text-center">
                         <input type="submit" value="登入">
                         <input type="reset" value="重置">
                     </td>
--- frontend/reg.php
+++ frontend/reg.php
@@ -1,36 +1,45 @@
 <h1 class="text-center mt-3 font-weight-bold">會員註冊</h1>
 
-<form action="./api/reg.php" method="post">
 
-        <div class="form-group text-center mt-5">
-            <label for="name" class="">用戶名:
-                <input class="form-control-md" type="text" name="name" id="name">
-            </label>
+
+
+<form action="./api/reg.php" method="post" class="was-validated">
+
+        <div class="form-group text-center mt-3 row col-12 col-md-4 mx-auto">
+            <label for="name">用戶名:</label>
+            <input class="form-control" type="text" name="name" id="name" required>
+            <div class="valid-feedback"></div>
+            <!-- <div class="invalid-feedback">Please fill out this field.</div> -->
         </div>
-        <div class="form-group text-center">
-            <label for="account">帳號:&nbsp;&nbsp;&nbsp;
-                <input class="form-control-md" type="text" name="account" id="account">
-            </label>
+        <div class="form-group text-center row col-12 col-md-4 mx-auto">
+            <label for="account">帳號:&nbsp;&nbsp;&nbsp;</label>
+            <input class="form-control" type="text" name="account" id="account" required>
+            <div class="valid-feedback"></div>
+            <!-- <div class="invalid-feedback">Please fill out this field.</div> -->
         </div>
-        <div class="form-group text-center">
-            <label for="password">密碼:&nbsp;&nbsp;&nbsp;
-                <input class="form-control-md" type="password" name="password" id="password">
-            </label>
+        <div class="form-group text-center row col-12 col-md-4 mx-auto">
+            <label for="password">密碼:&nbsp;&nbsp;&nbsp;</label>
+            <input class="form-control" type="password" name="password" id="password" required>
+            <div class="valid-feedback"></div>
+            <!-- <div class="invalid-feedback">Please fill out this field.</div> -->
         </div>
-        <div class="form-group text-center">
-            <label for="gender">性別:&nbsp;&nbsp;&nbsp;
-                <input class="form-control-md" type="text" name="gender" id="gender">
-            </label>
+        <div class="form-group text-center row col-12 col-md-4 mx-auto">
+            <label for="gender">性別:&nbsp;&nbsp;&nbsp;</label>
+            <input class="form-control" type="text" name="gender" id="gender" required>
+            <div class="valid-feedback"></div>
+            <!-- <div class="invalid-feedback">Please fill out this field.</div> -->
         </div>
-        <div class="form-group text-center">
-            <label for="email">信箱:&nbsp;&nbsp;&nbsp;
-                <input class="form-control-md" type="text" name="email" id="email">
-            </label>
+        <div class="form-group text-center row col-12 col-md-4 mx-auto">
+            <label for="email">信箱:&nbsp;&nbsp;&nbsp;</label>
+            <input class="form-control" type="text" name="email" id="email" required>
+            <div class="valid-feedback"></div>
+            <!-- <div class="invalid-feedback">Please fill out this field.</div> -->
         </div>
-        <div class="form-group text-center">
-            <label for="birthday">生日:&nbsp;&nbsp;&nbsp;
-                <input class="form-control-md" style="width:78%;" type="date" name="birthday" id="birthday">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-            </label>
+        <div class="form-group text-center row col-12 col-md-4 mx-auto">
+            <label for="birthday">生日:&nbsp;&nbsp;&nbsp;</label>
+            <input class="form-control"  type="date" name="birthday" id="birthday" required>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <div class="valid-feedback"></div>
+            <!-- <div class="invalid-feedback">Please fill out this field.</div> -->
         </div>
         <div class="form-group text-center">
                 <input class="btn btn-success" type="submit" value="送出">
